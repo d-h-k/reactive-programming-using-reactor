9101733784ef75cb1c675ec04981c848d00ad161
dongdongKing <ultraraptor@naver.com>
2023-01-23 12:39:15 +0900

    13강 끝

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -1,13 +1,52 @@
 package com.learnreactiveprogramming.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.util.List;
 
 import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class FluxAndMonoSchedulersService {
-
+    private static final Logger log = LoggerFactory.getLogger("FluxAndMonoSchedulersService");
     static List<String> namesList = List.of("alex", "ben", "chloe");
     static List<String> namesList1 = List.of("adam", "jill", "jack");
+    static List<String> myList = List.of("alex,ben,choi");
+
+
+    public static void main(String[] args) {
+        FluxAndMonoSchedulersService fmsService = new FluxAndMonoSchedulersService();
+        fmsService.namesFlux()
+                .subscribe(name -> log.info(name));
+
+        log.info("\n\n namesMono ----");
+        fmsService.namesMono()
+                .subscribe(name -> log.info(name));
+
+        log.info("\n\n Mono test----");
+//        fmsService.listMonoDong()
+//                .subscribe(myList.stream().forEach(s -> log.info(s)));
+//        이렇게는 안되는구나..
+
+
+    }
+
+    private Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+        //이런식으로 무한대의 요소를 처리 할 수 있음
+    }
+
+    public Mono<String> namesMono() {
+        return Mono.just("alex");
+        //하나만 가능
+    }
+
+    public Mono<List<String>> listMonoDong() {
+        return Mono.just(myList);
+    }
+
 
     private String upperCase(String name) {
         delay(1000);
--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,12 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorServiceTest {
+
+    public static void main(String[] args) {
+    }
+
 }
