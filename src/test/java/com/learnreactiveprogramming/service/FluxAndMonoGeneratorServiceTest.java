21de87318d6952a3972fa9ffa02ef0787181ab38
dongdongKing <ultraraptor@naver.com>
2023-01-23 12:53:51 +0900

    성공하는 검증테스트

--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,19 +1,20 @@
 package com.learnreactiveprogramming.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.util.List;
-
+@Slf4j
 public class FluxAndMonoGeneratorServiceTest {
 
     FluxAndMonoGeneratorService fms = new FluxAndMonoGeneratorService();
 
-    @DisplayName("")
+    @DisplayName("플렉스검증성공")
     @Test
-    public void flux() {
+    public void fluxSuccess() {
         //given
 
         //when
@@ -22,6 +23,19 @@ public class FluxAndMonoGeneratorServiceTest {
         StepVerifier.create(fluxName)
                 .expectNext("alex","ben", "chloe")
                 .verifyComplete();
+    }    @DisplayName("플렉스검증실패")
+    @Test
+    public void fluxFail() {
+        //given
+
+        //when
+        var fluxName = fms.namesFlux();
+        //then
+        StepVerifier.create(fluxName)
+                .expectNext("ben","alex", "chloe")
+                .verifyComplete();
+        //
+        log.warn("순서가 다르면 실패한다");
     }
 
 }
